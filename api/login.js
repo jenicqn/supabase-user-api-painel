dbc97e0bafaf42b0a56b027d616713bc45f455f5
jenicqn <129794684+jenicqn@users.noreply.github.com>
2025-06-02 17:24:32 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -6,7 +6,7 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://painel.smashdocabo.com');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // ⚠️ TEMPORÁRIO apenas para testes locais
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
