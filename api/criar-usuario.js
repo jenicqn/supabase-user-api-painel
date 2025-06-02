63a4d197012f43452297ab8b48001e87fc1d5c80
jenicqn <129794684+jenicqn@users.noreply.github.com>
2025-06-01 22:14:54 -0300

    Update criar-usuario.js

--- api/criar-usuario.js
+++ api/criar-usuario.js
@@ -7,6 +7,17 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+  return res.status(200).setHeader('Access-Control-Allow-Origin', '*')
+    .setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+    .setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    .end();
+}
+
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
