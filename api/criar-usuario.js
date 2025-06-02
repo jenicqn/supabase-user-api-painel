f849299bd4216a102985e98a21fbff9dd1bdf380
jenicqn <129794684+jenicqn@users.noreply.github.com>
2025-06-02 02:55:27 -0300

    Update criar-usuario.js

--- api/criar-usuario.js
+++ api/criar-usuario.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
     const { data, error } = await supabase.auth.admin.createUser({
       email,
       password: senha,
-      email_confirm: true
+      email_confirm: false
     });
 
     if (error) return res.status(400).json({ error: error.message });
